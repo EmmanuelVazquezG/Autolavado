152ad41c88c218b052a3863a242b4c9fbc50e39e
prado24 <pradojimenezmaria@gmail.com>
2024-05-20 14:06:22 -0600

    Base de BD

--- README.md
+++
@@ -1,1 +0,0 @@
-Link De Backlog del proyecto: https://gafes.atlassian.net/jira/software/projects/GAFFES/boards/1/backlog?epics=visible
--- controller/home.php
+++
@@ -1,9 +0,0 @@
-<?php 
-	session_start();
-	require 'libreria/IFormulario.php';
-	require 'libreria/FormularioFactory.php';
-	require 'libreria/FormularioHome.php';
-
-	$p = array();
-	View('home',$p);
- ?>
